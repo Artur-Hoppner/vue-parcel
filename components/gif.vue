ae4522c88af9fbd32472fad027ee14c6308de453
Artur <Artur.Hoppner@hotmail.com>
2020-09-11 22:16:38 +0200

    testing env mode with v-if, works

--- components/gif.vue
+++ components/gif.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <h1>show GIF</h1>
+    <h1 v-if="(getItRight = true)">
+      fuck it works. Good job 100%
+    </h1>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    console.log(process.env.NODE_ENV);
+  },
+  computed: {
+    getItRight() {
+      if (process.env.NODE_ENV == "production") {
+        return true;
+      }
+    },
+  },
+};
 </script>
 
-<style lang='scss'>
-</style>
+<style lang="scss"></style>
