293a8c8f67d5537ba33fbf38c2b8a10eb2e68784
Artur <Artur.Hoppner@hotmail.com>
2020-09-11 21:55:40 +0200

    functional production mode and started gif file

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>Env test parcel</p>
+    <h1>{{ name }}</h1>
+    <gif />
+  </div>
+</template>
+
+<script>
+import gif from "./components/gif";
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "testing data in app",
+    };
+  },
+  components: {
+    gif,
+  },
+  mounted() {
+    console.log(process.env.VUE_APP_ROOT_ENV);
+    console.log("testing some more");
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
